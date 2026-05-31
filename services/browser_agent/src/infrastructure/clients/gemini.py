2bbcbd3834e3ac8c5087995f315fa95bd772fe80
usvvdev <usv.dev@yandex.ru>
2026-05-31 18:00:26 +0300

    adeded new conf for caddyclear

--- services/browser_agent/src/infrastructure/clients/gemini.py
+++ services/browser_agent/src/infrastructure/clients/gemini.py
@@ -31,7 +31,7 @@ class Gemini(IGeminiProtocol):
             temperature=1.0,
             api_key=self._config.google_api_key,
             httpx_client=Client(
-                proxies={
+                proxy={
                     "http": self._config.proxy_url,
                     "https": self._config.proxy_url,
                 }
